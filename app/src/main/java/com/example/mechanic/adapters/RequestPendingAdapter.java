af1dc25969dad5e27fce5f5d7876a9df3162f70d
vikas <goyalvikas546@gmail.com>
2020-03-09 22:12:42 +0530

    pending request

--- app/src/main/java/com/example/mechanic/adapters/RequestPendingAdapter.java
+++ app/src/main/java/com/example/mechanic/adapters/RequestPendingAdapter.java
@@ -14,59 +14,47 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mechanic.R;
 import com.example.mechanic.model.Request;
+import com.firebase.ui.database.paging.DatabasePagingOptions;
+import com.firebase.ui.database.paging.FirebaseRecyclerPagingAdapter;
+import com.firebase.ui.database.paging.LoadingState;
 
 import java.util.List;
 
-public class RequestPendingAdapter extends  RecyclerView.Adapter<RequestPendingAdapter.MyHolder1>{
+
+public class RequestPendingAdapter extends FirebaseRecyclerPagingAdapter<Request, RequestPendingAdapter.MyHolder> {
 
     Context c;
-    List<Request> x ;      //Define your list here    - Aditya
-
-    public RequestPendingAdapter(Context c, List<Request> x)                                               //Enter the type of data in the space for model
-    {
+    /**
+     * Construct a new FirestorePagingAdapter from the given {@link DatabasePagingOptions}.
+     *
+     * @param options
+     */
+    public RequestPendingAdapter(DatabasePagingOptions<Request> options, Context c) {
+        super(options);
         this.c = c;
-        this.x = x;
+
     }
 
-    @NonNull
-    @Override
-    public RequestPendingAdapter.MyHolder1 onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-
+    public RequestPendingAdapter.MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.pending_request_item,null);
-        return new RequestPendingAdapter.MyHolder1(view);
+        return new RequestPendingAdapter.MyHolder(view);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull RequestPendingAdapter.MyHolder1 myholder1, int position) {
-
-
-        myholder1.responsiblemanName.setText(x.get(position).getComplaint().getManager().getUserName());
-        myholder1.description.setText(x.get(position).getDescription());
-        myholder1.complain_id.setText((int) x.get(position).getComplaint().getComplaintId());
-        myholder1.request_id.setText(String.valueOf((int) x.get(position).getRequestId()));
-
-        Log.i("asdf","fgh");
-
-//        boolean isExpanded = x.get(position).isExpanded();
-        boolean isExpanded = true;
-        myholder1.ll_hide.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+    protected void onBindViewHolder(RequestPendingAdapter.MyHolder viewHolder, int position, Request model) {
+        viewHolder.bind(model);
+    }
+    protected void onLoadingStateChanged(LoadingState state){
 
     }
 
-    @Override
-    public int getItemCount() {
-        return x.size();                                                                                   // Return item count from firebase
-    }
-
-
-    class MyHolder1 extends RecyclerView.ViewHolder{
-
-
+    class MyHolder extends RecyclerView.ViewHolder
+    {
         TextView request_id , responsiblemanName ,  description , complain_id ;
         CardView cardview;
         LinearLayout ll_hide;
 
-        public MyHolder1(@NonNull View itemView) {
+        public MyHolder(final View itemView)
+        {
             super(itemView);
 
             request_id = itemView.findViewById(R.id.s_RecyclerView_requestID__pen_req);
@@ -87,7 +75,22 @@ public class RequestPendingAdapter extends  RecyclerView.Adapter<RequestPendingA
                         ll_hide.setVisibility(View.GONE);
                 }
             });
-        }
-    }
 
+        }
+
+        public void bind(Request model) {
+            responsiblemanName.setText(model.getComplaint().getManager().getUserName());
+            description.setText(model.getDescription());
+            complain_id.setText(String.valueOf(model.getComplaint().getComplaintId()));
+            request_id.setText(String.valueOf(model.getRequestId()));
+
+            Log.i("asdf","fgh");
+
+    //        boolean isExpanded = x.get(position).isExpanded();
+            boolean isExpanded = true;
+            ll_hide.setVisibility(isExpanded ? View.VISIBLE : View.GONE);
+        }
+
+
+    }
 }
